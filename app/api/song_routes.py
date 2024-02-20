d918792750d2d6ed3d139f9d2721e95415da1d28
Trevor <trevirv@gmail.com>
2024-02-20 05:26:30 -0800

    comments for put, post, delete

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
-from app.models import Song, db
+from app.models import Song, db, Comment
 from flask_login import login_required, current_user
 from datetime import datetime
+import app.s3_helpers as s3
 
 song_routes = Blueprint('songs', __name__)
 
@@ -20,8 +21,8 @@ def songs():
 def single_song(id):
     song = Song.query.get(id)
 
-    # return song.song_dict()
-    return print("hi")
+    return song.song_dict()
+
 
 #Upload a song
 @song_routes.route('', methods=["POST"])
@@ -29,6 +30,21 @@ def single_song(id):
 def post_song():
     #song_link = aws
     #song_pic = aws
+    if "song" not in request.files:
+        return {"errors": "song required"}, 400
+    
+
+
+    song = request.files['song']
+
+    if not s3.song_file(song.filename):
+        return {"errors": "file type not supported"}, 400
+
+    song.filename = s3.get_unique_filename(song.filename)
+
+    upload = s3.upload_file_to_s3(song)
+
+    song_url = upload['url']
 
     song_data = request.form.to_dict()
 
@@ -39,17 +55,13 @@ def post_song():
         visibility = song_data["visibility"],
         plays = song_data["plays"],
         user_id = current_user.id,
-        created_at = datetime.utcnow()
+        created_at = datetime.utcnow(),
+        song_link = song_url
     )
 
-
-
-
-
-
-
-
-
+    db.session.add(new_song)
+    db.session.commit()
+    return new_song()
 
 #Edit a song by song id
 #Need AWS framework implemented
@@ -97,3 +109,51 @@ def delete_song(id):
     db.session.commit()
 
     return {"Message": "Successfully Deleted"}
+
+# Post a new comment on the song
+@song_routes.route('/<int:id>/comments', methods=["POST"])
+@login_required
+def post_song_comment(id):
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        comment = Comment(
+            comment = form.data["comment"],
+            user_id = current_user.id,
+            song_id = id,
+            song_time = form.data["song_time"],
+            created_at = datetime.utcnow(), 
+            updated_at = datetime.utcnow()
+        )
+        db.session.add(comment)
+        db.session.commit()
+        return comment.comment_dict()
+    return {"Error": "Unable to create comment"}
+
+# Delete a comment from the song
+@song_routes.route('/<int:_id>/comments/<int:c_id>', methods=["DELETE"])
+@login_required
+def delete_song_comment(_id, c_id):
+    comment = Comment.query.get(c_id)
+    if not comment:
+        return {"Error": "Cannot find comment"}
+    db.session.delete(comment)
+    db.session.commit()
+    return {"message": "Deleted successful"}
+
+# Edit a comment
+@song_routes.route('/<int:_id>/comments/<int:c_id>', methods=["PUT"])
+@login_required
+def edit_song_comment(_id, c_id):
+    comment = Comment.query.get(c_id)
+    form = CommentForm()
+    if comment.user_id == current_user.id:
+        comment.comment = form.data['comment']
+        comment.song_time = form.data['song_time']
+        comment.updated_at = datetime.utcnow()
+        db.session.add(comment)
+        db.session.commit()
+        return comment.comment_dict()
+    else:
+        return {"Error": "Could not edit comment"}
