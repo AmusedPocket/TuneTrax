3548a5f97d9308fbfe899a2e2a2866ec1e183726
ChristopherAumonte <christopheraumonte@gmail.com>
2024-02-19 19:16:43 -0500

    album store reducer

---
+++ react-vite/src/redux/album.js
@@ -0,0 +1,39 @@
+import {normalizeObj} from "./helpers"
+const GET_ALBUM= 'album/GET_ALBUM'
+
+export const getAlbums = (albums) => {
+    return {
+        type: GET_ALBUM,
+        albums
+    }
+}
+
+export const thunkGetAlbum = (albumId) => async (dispatch)=> {
+ const res = await fetch(`/api/albums/${albumId}`)
+ if (res.ok){
+    const album = await res.json()
+    dispatch(getAlbums(album))
+ } else {
+    const data = await res.json()
+    if(data.errors){
+        return data;
+    }
+ }
+}
+
+const initialState = {}
+
+const albumReducer = (state = initialState, action)=> {
+    let newState;
+    switch (action.type){
+        case GET_ALBUM:
+            newState = { ...state}
+            newState.albums = normalizeObj(action.albums)
+            return newState
+
+        default:
+            return state;
+    }
+}
+
+export default albumReducer
