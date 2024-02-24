b450c96cbea480ac61a1611f605b1502aad40f32
Trevor <trevirv@gmail.com>
2024-02-24 11:30:06 -0800

    styling for front page started

--- react-vite/src/components/HomePage/SingleAlbum.jsx
+++ react-vite/src/components/HomePage/SingleAlbum.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './SingleAlbum.css'
 
 function SingleAlbum({ album }) {
     
@@ -8,19 +9,19 @@ function SingleAlbum({ album }) {
             res.push(songs[i]);
         return res;
     }
-
+    console.log(album)
     return (
-        <div>
+        <div className="single-album-container">
             <Link to={`/albums/${album.id}`}>
-                <img src={album.album_pic} alt={`${album.title} album picture`} />
+                <img src={album.album_pic} alt={`${album.title} album picture`} className="single-album-album"/>
             </Link>
             <div>
                 {firstFiveSongs(album.songs).map(song => (
                     <div key={song.id}>
                         <Link to={`/songs/${song.id}`}>
-                            <img src={song.song_pic} alt={`${song.title} song picture`} />
+                            <img src={song.song_pic} alt={`${song.title} song picture`} className="single-album-song-picture"/>
                         </Link>
-                        <span>{song.username} -- {song.title}</span>
+                        <span className="first-five-album-song-text">{song.username} -- {song.title}</span>
                     </div>
                 ))}
             </div>
